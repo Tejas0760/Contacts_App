c2883476ed16b78cb73b999a371892ce2f8956ed
Tejas Choudhary <162564764+Tejas0760@users.noreply.github.com>
2024-09-24 15:10:07 +0530

    Add files via upload

---
+++ src/main/java/com/ContactsApp/ContactApp/controllers/ApiController.java
@@ -0,0 +1,27 @@
+package com.ContactsApp.ContactApp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ContactsApp.ContactApp.entities.Contact;
+import com.ContactsApp.ContactApp.services.ContactService;
+
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    // get contact
+
+    @Autowired
+    private ContactService contactService;
+
+    @GetMapping("/contacts/{contactId}")
+    public Contact getContact(@PathVariable String contactId) {
+        return contactService.getById(contactId);
+    }
+
+}
